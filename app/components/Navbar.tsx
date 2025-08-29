c05d1200ad83599cfc2d6860b4c634f781f5afb0
Bumbaclat89 <HANY89@LIVE.SE>
2025-08-29 12:07:16 +0200

    added logout function

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import {Link} from "react-router";
+import React from "react";
+import { Link, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-        <Link to ="/">
-            <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    navigate("/auth?next=/");
+  };
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+      </Link>
+      <div className="flex items-center gap-3">
+        <Link to="/upload" className="primary-button w-fit">
+          Upload Resume
         </Link>
-            <Link to ="/upload" className="primary-button w-fit">
-                Upload Resume
-            </Link>
-        </nav>
-    )
-}
-export default Navbar
+        {auth.isAuthenticated && (
+          <button className="danger-button w-fit" onClick={handleLogout}>
+            Logout
+          </button>
+        )}
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
